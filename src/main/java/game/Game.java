16e6941b525db6e527c8cf874f9c2e83a0a3dd31
Adam <adamharald440@gmail.com>
2020-11-23 17:32:27 +0100

    Added Maven support

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -12,7 +12,7 @@ public class Game {
     static Beaker beaker;
     static GameBoard gameBoard;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Scanner scan = new Scanner(System.in);
 
