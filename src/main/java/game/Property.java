81db7393b682195683918d998bcf4244f3189055
Adam <adamharald440@gmail.com>
2020-11-25 09:50:12 +0100

    Added owner attribute, which by default will be the bank

--- src/main/java/game/Property.java
+++ src/main/java/game/Property.java
@@ -7,12 +7,18 @@ public class Property extends Field {
     // Attributes
     private final Color color;
     private final int relatedPropertyPosition;
+    private Actor owner;
 
     // Constructor
     public Property(String title, String subText, String description, int position, Color color, int relatedPropertyPosition) {
         super(title, subText, description, position);
         this.color = color;
         this.relatedPropertyPosition = relatedPropertyPosition;
+        // Set the default owner to be the bank, when property is purchased, players buy it from the bank
+    }
+
+    public void sellProperty(Actor buyer) {
+
     }
 
     public Color getColor() {
@@ -21,5 +27,13 @@ public class Property extends Field {
 
     public int getRelatedPropertyPosition() {
         return relatedPropertyPosition;
+    }
+
+    public Actor getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Actor owner) {
+        this.owner = owner;
     }
 }
