791ca474e7c96b44e1cbc0eadf593c4f2d6dbfbe
Bastian <basbafala@gmail.com>
2020-11-25 15:53:44 +0100

    added method for making transactons to Actor and erased redundant method from Account

--- src/main/java/game/Account.java
+++ src/main/java/game/Account.java
@@ -5,7 +5,10 @@ public class Account {
     private int balance;
 
     // Constructor, sets starting balance
-    public Account (int startBalance) { balance = startBalance; }
+    public Account (int startBalance)
+    {
+        balance = startBalance;
+    }
 
     // Get balance
     public int getBalance () { return this.balance; }
@@ -13,16 +16,7 @@ public class Account {
     // Set balance
     public void setBalance (int newBalance) { this.balance = newBalance; }
 
-    // Make transaction on the account - and prevent negative balance. Returns true if transaction is successful.
-    public boolean makeTransaction (int value) {
-        if (this.balance >= -value) {
-            this.balance += value;
-            return true;
-        }
-        else {
-            System.out.println("There is an insufficient balance on the account to make the transaction.");
-            return false;
-        }
+
     }
 
 }
