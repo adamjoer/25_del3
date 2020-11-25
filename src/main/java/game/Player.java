99ec534e015fc5f6fcaba84b0b7fdc09836ba81b
Bastian <basbafala@gmail.com>
2020-11-25 12:17:18 +0100

    Removed xtra turn from player and added inheritance

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -1,11 +1,11 @@
 package game;
 
-public class Player {
+public class Player extends Actor{
     //Variable
     final private String name;
     final private Account account;
     private int position = -1;
-    private boolean extraTurn = false;
+
 
     //Constructor: Defining variables
     public Player(String inputName, int startBalance) {
@@ -15,16 +15,12 @@ public class Player {
 
     // Relevant setters
     public int getBalance() { return this.account.getBalance(); }
-    public String getName() {
-        return this.name;
-    }
+    public String getName() { return this.name; }
     public int getPosition() { return this.position; }
-    public boolean getExtraTurn() { return this.extraTurn; }
 
     // Relevant setters
     public void setBalance(int newBalance) { this.account.setBalance(newBalance); }
     public void setPosition(int newPosition) { this.position = newPosition; }
-    public void setExtraTurn(boolean b) { this.extraTurn = b; }
 
     // Attempt to make a transaction, returns true if successful.
     public boolean makeTransaction(int points) { return this.account.makeTransaction(points); }
