5a30bf3c47f1daa9d87c1aec0abed2a73089717c
Martin <s195127@student.dtu.dk>
2020-11-25 14:15:50 +0100

    ActorController initiated player array and defined start balance according to players.

--- src/main/java/game/ActorController.java
+++ src/main/java/game/ActorController.java
@@ -1,4 +1,39 @@
 package game;
 
-public class ActorController {
+public class ActorController
+{
+    //Defining variables
+    private int number_player;
+    private final Player[] players;
+    private int startBalance;
+    //Momentary name
+    private String nametemp;
+
+    public ActorController(int number_player)
+    {
+
+        this.number_player = number_player;
+        players = new Player[number_player];
+
+        if (number_player == 4)
+         {
+            startBalance = 16;
+         }
+        else if (number_player == 3)
+        {
+            startBalance = 18;
+        }
+        else {
+            startBalance = 20;
+        }
+        for (int i = 0; i < number_player; i++)
+        {
+            players[i] = new Player(nametemp,startBalance);
+        }
+    }
+    public void makeTransaction ()
+    {
+
+    }
+
 }
