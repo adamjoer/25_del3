653c8c9b26b72a1e1b9485eb956c46514c2d969a
Andreas Carlsen <s205437@student.dtu.dk>
2020-11-25 09:45:22 +0100

    Added shuffle and draw methods to ChanceCardController

--- src/main/java/game/ChanceCardController.java
+++ src/main/java/game/ChanceCardController.java
@@ -1,4 +1,34 @@
 package game;
 
+import java.awt.*;
+import java.util.Random;
+
 public class ChanceCardController {
+    private static ChanceCard[] drawPile;
+    private static int cardsDrawn = 0;
+
+    public ChanceCardController(){
+        createPile();
+    }
+
+
+
+    public ChanceCard drawChanceCard() {
+        return drawPile[cardsDrawn++];
+    }
+
+    public static void shufflePile(){
+        Random rand = new Random();
+
+        for (int i = 0; i < drawPile.length; i++) {
+            int randomIndexToSwap = rand.nextInt(drawPile.length);
+            ChanceCard temp = drawPile[randomIndexToSwap];
+            drawPile[randomIndexToSwap] = drawPile[i];
+            drawPile[i] = temp;
+        }
+
+    }
+
+
+
 }
