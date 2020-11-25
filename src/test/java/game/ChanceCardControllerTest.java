e4bf47e15ff46a9eae264e467eb16327114ab98c
Andreas Carlsen <s205437@student.dtu.dk>
2020-11-25 14:43:42 +0100

    Refactoring

--- src/test/java/game/ChanceCardControllerTest.java
+++ src/test/java/game/ChanceCardControllerTest.java
@@ -7,9 +7,7 @@ class ChanceCardControllerTest {
     public static void main(String[] args){
         ChanceCardController controller = new ChanceCardController();
 
-        System.out.println();
-        System.out.println();
-        System.out.println("Single card: ------------------");
+
         System.out.println(controller.drawChanceCard().getChanceCardText());
         System.out.println(controller.drawChanceCard().getChanceCardText());
         System.out.println(controller.drawChanceCard().getChanceCardText());
@@ -18,10 +16,10 @@ class ChanceCardControllerTest {
 
         System.out.println();
         System.out.println("After shuffle:");
-        System.out.println("Single card: ------------------");
         System.out.println(controller.drawChanceCard().getChanceCardText());
         System.out.println(controller.drawChanceCard().getChanceCardText());
         System.out.println(controller.drawChanceCard().getChanceCardText());
+
     }
 
 }
