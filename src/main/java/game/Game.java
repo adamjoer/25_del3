203e75c8c4081007724bcbd76ff44e5920ad5e6a
Adam <adamharald440@gmail.com>
2020-11-26 17:48:36 +0100

    Started on main method

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -2,7 +2,14 @@ package game;
 
 public class Game {
 
+    final static int DICE = 2;
+    final static int DIE_MAXVALUE = 6;
+    private static Beaker beaker;
+    private static GameBoard board;
+
     public static void main(String[] args) {
+        beaker = new Beaker(DICE, DIE_MAXVALUE);
+        board = new GameBoard();
 
     }
 }
