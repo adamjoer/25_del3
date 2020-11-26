579a43bbee54594b69f09322c9675f0b41baa4c9
Bastian <basbafala@gmail.com>
2020-11-26 15:41:57 +0100

    created test of movePlayer method

--- src/test/java/game/PlayerTest.java
+++ src/test/java/game/PlayerTest.java
@@ -1,8 +1,10 @@
 package game;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-public class PlayerTest {
-
+public class PlayerTest extends Actor {
+    /*
      @org.junit.jupiter.api.Test
     void createPlayerCheckName(){
         String playerName = "Bo";
@@ -15,5 +17,34 @@ public class PlayerTest {
         int accBalance = 500;
         Player player = new Player("Bo", accBalance);
         assertEquals(accBalance, player.getBalance());
+    }
+
+     */
+
+    @Test
+    void testMakeTransaction() {
+
+
+
+    }
+
+    @Test
+    void movePlayer() {
+
+        String name = "bo";
+        Player  player = new Player(100, name);
+        Beaker beaker = new Beaker(1,6);
+
+
+
+        for (int i = 0; i < 1000; i++) {
+            beaker.roll();
+            int rolled = beaker.getSum();
+            player.movePlayer(rolled);
+            assertTrue(player.getCurrentPosition() != player.getPreviousPosition() && player.getCurrentPosition() < 25);
+        }
+
+
+
     }
 }
