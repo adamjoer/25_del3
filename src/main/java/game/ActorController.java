ce5a92d1518dfd6d611c598397399f10c256118f
Adam <adamharald440@gmail.com>
2020-11-27 07:51:29 +0100

    Changed makeTransaction so that the sender's balance goes to 0 if they can't pay

--- src/main/java/game/ActorController.java
+++ src/main/java/game/ActorController.java
@@ -1,13 +1,11 @@
 package game;
 
-public class ActorController
-{
+public class ActorController {
     //Defining variables
 
     private final Actor[] actors;
     private final int startBalance;
-    private final int bankBalance = 90;
-    private final String[] playerNames;
+    private final int BANK_BALANCE = 90;
     //Momentary name
     private String name;
 
@@ -15,30 +13,23 @@ public class ActorController
     /**
      * @param playerNames
      */
-    public ActorController(String[] playerNames)
-    {
+    public ActorController(String[] playerNames) {
 
         //Defining number of players.
-        this.playerNames = playerNames;
         actors = new Actor[playerNames.length + 1];
 
         //Calculating each players starting balance, based on the number of players
-        if (playerNames.length == 4)
-         {
+        if (playerNames.length == 4) {
             startBalance = 16;
-         }
-        else if (playerNames.length == 3)
-        {
+        } else if (playerNames.length == 3) {
             startBalance = 18;
-        }
-        else {
+        } else {
             startBalance = 20;
         }
         //Assigning player names and bank with their starting balance
-        actors[0] = new Bank(bankBalance);
-        for (int i = 1; i < actors.length; i++)
-            {
-                actors[i] = new Player(startBalance, playerNames[i]);
+        actors[0] = new Bank(BANK_BALANCE);
+        for (int i = 1; i < actors.length; i++) {
+            actors[i] = new Player(startBalance, playerNames[i]);
         }
     }
 
@@ -50,13 +41,15 @@ public class ActorController
      * @return
      */
     //makes transaction between two actors and returns false if senders balance is below amount
-    public boolean makeTransaction (int sender, int receiver, int amount) {
-        if(actors[sender].makeTransaction(-amount) && amount <91){
+    public boolean makeTransaction(int sender, int receiver, int amount) {
+        if (actors[sender].makeTransaction(-amount) && amount < 91) {
             actors[receiver].makeTransaction(amount);
             return true;
-        }
 
-        else { return false;}
+        } else {
+            actors[sender].setBalance(0);
+            return false;
+        }
     }
 
 
@@ -65,8 +58,7 @@ public class ActorController
      * @param Increment
      */
     //Calls Player class method with actor array
-    public void movePlayer(int Player, int Increment)
-    {
+    public void movePlayer(int Player, int Increment) {
         ((Player) actors[Player]).movePlayer(Increment);
     }
 
